d55f7ecfe387e6e6603c800658a748953e8beadc
Bernietolentinojr <50653306+Bernietolentinojr@users.noreply.github.com>
2019-05-15 17:38:05 +1000

    Update DataOptimisation.m

--- DataOptimisation.m
+++ DataOptimisation.m
@@ -1,2 +1,22 @@
-%%% Project for Intro into Data for Assignment 3
+clear all;
 
+NumTrials=100;
+LearningRate=0.001;
+numLoops=2000;
+
+CostTable= xlsread('CostTable.xlsx');
+CostTable=CostTable(2:9,2:13);
+
+CostTrials=zeros(NumTrials,1);
+
+PV=ones(1,36)*0.5;
+
+Epoch=zeros(NumTrials,36);
+
+CostBin2Dec = @(CostIndexBin) ...        % This is an anonymous function to  
+                CostIndexBin(1)*4 + ...  % get the decimal value from the 
+                CostIndexBin(2)*2 + ...  % 4 binary bits
+                CostIndexBin(3);
+ConcentorConnections = zeros(8,1);
+
+            
