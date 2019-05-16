125fa283e7655d242281b13574eb9dc86adb3dfd
meghanavanapam <13372596@student.uts.edu.au>
2019-05-16 17:55:47 +1000

    Update DataOptimisation.m

--- DataOptimisation.m
+++ DataOptimisation.m
@@ -1,18 +1,18 @@
-clear all; %set up all
+%set up all
+clear all; 
 
 %import cost table
 CostTable=xlsread('CostTable.xlsx');
-%CostTable=CostTable(2:9,2:13); %remove unnecessary rows and column numbers
+%remove unnecessary rows and column numbers
+CostTable=CostTable(2:9,2:13); 
 
 N = 12; % number of teminals
 
+%Need to run over a trail period
+% These parameters can be changed
 NumTrials= 50;
 LearningRate=0.001;
-NumberofLoop=200;
-
-CostTable=xlsread('CostTable.xlsx');
-CostTable=CostTable(2:8,2:12);
-CostTrials=zeros(NumTrials,1);
+NumberofLoops=200;
 
 % Let's start with the PV and then move on to the BS. We syart the PV with
 % values of 0.5 because the first Epoch of trials should have equally
@@ -23,14 +23,36 @@ ProbVector = ProbVector*0.5;     % This sets all the values to 0.5
 
 ConConnects = zeros(8,1); % Empty array with each element representing a concentrator
 
-for main = 
-
-
-
-
-
-CostBin2Dec = @(CostIndexBin) ...        % This is an anonymous function to  
-                CostIndexBin(1)*8 + ...  % get the decimal value from the 
-                CostIndexBin(2)*4 + ...  % 4 binary bits
-                CostIndexBin(3)*2 + ...
-                CostIndexBin(4)*1;
+for Main = 1:NumberofLoops
+    
+    % Create empty array to store total cost of each trial
+    TrialsCost = zeros(NumTrials, 1);
+    % Create an epoch by creating an empty array.
+    Epoch = zeros(NumTrials, 36);
+    
+    % Calculate the cost of each trial
+    % vector in order to achieve minimum cost.
+    for EpochLoop = 1:NumTrials
+        ConConnects  = zeros(8,1);      % each element representing a concentrator.
+                                        
+        % Create a Random Binary array 12 by 3. 
+        RandomBinary = rand([0 1], 1, 36);
+        Epoch(EpochLoop, :) = RandomBinary < ProbVector;
+        
+        RandomBinary = reshape(Epoch(EpochLoop, :), 12,3);
+        
+        % Create binary matrix (12 by 3)
+        for RowNum = 1:N
+            CostTableRow(RowNum) = ConvertBinary2Number(RandomBinary(RowNum, :));
+            ConConnects(CostTableRow(RowNum)+ 1) = ConConnects(CostTableRow(RowNum)+1) +1;
+        end  
+        
+        %Checking to see if more than three terminals are connected to one
+        %concentrator.
+        % If so, assign TrailCost with a large number so that the trail is
+        % not considered.
+        for ConnectorNum = 1:8
+            if ConConnects(ConnectorNum, 1) > 3
+                TrialsCost(EpochLoop, 1) >= 10000;
+            end
+        end
