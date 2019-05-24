2538a29db24e8500c67cab00e31836d46599fc85
rlcaust <Jayden@rlc.com.au>
2019-05-24 20:56:29 +1000

    Update PBIL

--- PBIL.m
+++ PBIL.m
@@ -1,51 +1,67 @@
-%set up all
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Wrote by Jayden Lee
+%Date: 24/05/19
+%Instead of using a Binary String, we are going to use a matrix.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all; 
 
-%import cost table
-CostTable=xlsread('CostTable.xlsx');
-%remove unnecessary rows and column numbers
-CostTable=CostTable(2:9,2:13); 
+%% Anonymous Function that converts bits to decimal value
 
-N = 12; % number of teminals
+costBin2Dec = @(costIndexBin) ...
+costIndexBin(1)*4 + costIndexBin(2)*2 + costIndexBin(3);
 
-%Need to run over a trail period
-% These parameters can be changed
+
+%% Customisation Parameters: 
+
+% Number of Trials running
 NumTrials= 50;
+
+% Learning Increment Rate
 LearningRate=0.001;
+
+% Number of Loops
 NumberofLoops=200;
 
-% Let's start with the PV and then move on to the BS. We syart the PV with
-% values of 0.5 because the first Epoch of trials should have equally
-% likely 1 and zero.
+% End of Customisation Parameters
 
-ProbVector = ones(1,40); % This gives us an array 40 1's
-ProbVector = ProbVector*0.5;     % This sets all the values to 0.5
 
-ConConnects = zeros(8,1); % Empty array with each element representing a concentrator
+%% Array Generation:
 
+% Probability Vector
+ProbVector = ones(1,36)*0.5; % Default 0.5 Probabiity Vector
+% Connection Enabled Array
+ConConnects = zeros(1,36); % Empty array with each element representing a concentrator
+%Epoch Cost Storage
+EpochCost = zeros(NumberofLoops, 1);
+%Cost Table
+CostTable=xlsread('CostTable.xlsx');
+%Remove the Unnessecrary Rows and Columns From Import
+CostTable=CostTable(2:9,2:13); 
+
+
+%% End of Array Generation
+
+%% Main Loop
 for Main = 1:NumberofLoops
     
     % Create empty array to store total cost of each trial
-    TrialsCost = zeros(NumTrials, 1);
+    TrialsCost = zeros(NumberofLoops, 1);
     % Create an epoch by creating an empty array.
-    Epoch = zeros(NumTrials, 36);
+    Epoch = zeros(NumberofLoops, 1);
     
+    for EpochLoop = 1:NumTrials
+        Epoch(EpochLoop,:) = rand(1,36)<probabilityVector;
+    end
+
     % Calculate the cost of each trial
     % vector in order to achieve minimum cost.
     for EpochLoop = 1:NumTrials
         ConConnects  = zeros(8,1);      % each element representing a concentrator.
                                         
         % Create a Random Binary array 12 by 3. 
-        RandomBinary = rand([0 1], 1, 36);
-        Epoch(EpochLoop, :) = RandomBinary < ProbVector;
+        RandomBinary = rand([0 1], 8, 12);    
+    end  
         
-        RandomBinary = reshape(Epoch(EpochLoop, :), 12,3);
-        
-        % Create binary matrix (12 by 3)
-        for RowNum = 1:N
-            CostTableRow(RowNum) = ConvertBinary2Number(RandomBinary(RowNum, :));
-            ConConnects(CostTableRow(RowNum)+ 1) = ConConnects(CostTableRow(RowNum)+1) +1;
-        end  
-        
-    end
+    
+end
     
